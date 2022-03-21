f7a5248d22a2b09fccde18bd62d15d92041dd6c7
anasmak03 <anas11.makhloufi@gmail.com>
2022-03-21 21:21:17 +0100

    router

---
+++ routes/authRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const { requireLogin } = require("../middleware/auth")
+const  User  = require('../model/User');
+
+const {register,login} = require('../controllers/authControllers');
+
+
+router.post('/register', register)
+router.post('/login', login)
+
+router.get("/", requireLogin, async (req, res) => {
+    console.log(req.user)
+    try {
+      const user = await User.findById(req.user._id).select("-password")
+      res.json(user)
+    } catch (err) {
+      console.log(err)
+    }
+  })
+  
+  
+  
+module.exports = router
